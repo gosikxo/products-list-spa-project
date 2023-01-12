d6a20780032f6dba37039b8bea1445de8982684e
Gosia Brozek <gosikbrozek1997@gmail.com>
2023-01-12 19:48:53 +0100

    Search product by id

--- src/App.tsx
+++ src/App.tsx
@@ -10,25 +10,79 @@ export type Product = {
 }
 
 function App() {
-  const [products, setProducts] = useState<Product[] | null>(null)
+  const [products, setProducts] = useState<Product[]>([])
   const [pageNumber, setPageNumber] = useState<number>(1)
+  const [searchInput, setSearchInput] = useState("")
+  const [error, setError] = useState<string | null>(null);
+  const [totalPages, setTotalPages] =useState<number>(1);
+
+  const handleChange = (event: React.SyntheticEvent): void => {
+    let target = event.target as HTMLInputElement
+    if (target.value.length === 0) {
+      setSearchInput("");
+    }
+    const parsedNumber = parseInt(target.value);
+    if (isNaN(parsedNumber)) {
+      return;
+    }
+    setSearchInput(target.value)
+  }
+
   useEffect(() => {
-    fetch(`https://reqres.in/api/products?page=${pageNumber}`)
-      .then(res => res.json())
-      .then(res => {
-        setProducts(res.data)
-      })
-  }, [pageNumber])
+    // if we're searching
+    if (searchInput.length > 0) {
+      fetch(`https://reqres.in/api/products?id=${searchInput}`)
+        .then(res => res.json())
+        .then(res => {
+          setError(null);
+
+          if (!res.data) {
+            setError("Couldn't find product on server.")
+            return;
+          }
+          if (Array.isArray(res.data)) {
+            setProducts(res.data)
+          } else {
+            setProducts([res.data])
+          }
+        })
+    } else {
+      // if we show list of products
+      fetch(`https://reqres.in/api/products?page=${pageNumber}`)
+        .then(res => res.json())
+        .then(res => {
+          setError(null);
+
+          if (!res.data) {
+            setError("Couldn't load data from server.")
+            return;
+          }
+
+          if (res.data) { 
+            setProducts(res.data)
+            setTotalPages(res.total_pages)
+          } else {
+
+          }
+        })
+    }
+  }, [pageNumber, searchInput])
 
   function changePage() {
     pageNumber === 1 ? setPageNumber(2) : setPageNumber(1)
   }
 
+  const isSearching = searchInput.length > 0;
+
   return (
     <div className="App">
-      {products !== null ? <Table products={products} /> : null}
-      {pageNumber === 2 ? <button className="pageButton" onClick={changePage}>Previous Page</button> : null}
-      {pageNumber === 1 ? <button className="pageButton" onClick={changePage}>Next Page</button> : null}
+      <label>search</label><input type="text" value={searchInput} onChange={handleChange}></input>
+      {error && <div>{error}</div>}
+      {!error && products !== null ? <Table products={products} /> : null}
+      { !isSearching && <div style={{display: "flex", justifyContent:"space-between"}}>
+        <button style={{display: "inline-flex"}} disabled={pageNumber === 1} className="pageButton" onClick={changePage}>Previous Page</button>
+        <button style={{display: "inline-flex"}} disabled={pageNumber === totalPages} className="pageButton" onClick={changePage}>Next Page</button>
+      </div>}
     </div>
   );
 }
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -3,24 +3,9 @@ import { Product } from '../App'
 
 
 export const Table = (props: { products: Product[] }) => {
-    const [SearchInput, setSearchInput] = useState("0")
-
-    const handleChange = (event: React.SyntheticEvent): void => {
-        let target = event.target as HTMLInputElement;
-        setSearchInput(target.value)
-    }
-
-    const handleClick = (): void => {
-
-    }
     return (
         <>
-            <input type="text" value={SearchInput} onChange={handleChange}>
-
-            </input>
-            <button onClick={handleClick}>
-                Search
-            </button>
+            
             <table>
                 <tbody>
                     <thead>
@@ -33,8 +18,10 @@ export const Table = (props: { products: Product[] }) => {
                         <td> name </td>
                         <td> year </td>
                     </tr>
-                    {
-                        props.products.map(product => <tr>
+                    { props.products
+                       
+                        .map(product =>
+                        <tr>
                             <td> {product.id} </td>
                             <td> {product.name} </td>
                             <td> {product.year} </td>
